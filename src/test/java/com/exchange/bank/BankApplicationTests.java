4b68610396aad5a6d10540f4e9aa65ebff09757a
Arzuv Bahramov <arzuv.bahramov@innowise.com>
2024-05-07 21:10:28 +0300

    ### Added test containers

--- src/test/java/com/exchange/bank/BankApplicationTests.java
+++ src/test/java/com/exchange/bank/BankApplicationTests.java
@@ -2,9 +2,26 @@ package com.exchange.bank;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class BankApplicationTests {
+
+	@SuppressWarnings("resource")
+	@ServiceConnection
+	@Container
+	public static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:latest")
+			.withDatabaseName("bank_exchange")
+			.withUsername("postgres")
+			.withPassword("postgres");
+
+	static {
+		postgres.start();
+	}
 
 	@Test
 	void contextLoads() {
